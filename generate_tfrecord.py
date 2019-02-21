faaf19641ad248aa92515507e2bbe5ff94f722dc
aloysiuschan <42513983+aloysiuschan@users.noreply.github.com>
2019-02-21 23:10:30 +0800

    Add files via upload

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -29,7 +29,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'smile':
+    if row_label == 'face':
         return 1
     else:
         None
